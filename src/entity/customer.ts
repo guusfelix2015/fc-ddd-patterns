1575262b31d99b88bb1d0f2719b9e4ca987b47ad
guusfelix2015 <gusfelix2015@gmail.com>
2024-07-12 23:10:24 -0300

    princípio de autovalidação

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,20 +2,34 @@ class Customer {
   _id: string;
   _name: string;
   _address: string;
-  _active: boolean;
+  _active: boolean = false;
 
   constructor(id: string, name: string, address: string, active: boolean) {
     this._id = id;
     this._name = name;
     this._address = address;
     this._active = active;
+    this.validate();
   }
 
   changeName(name: string) {
     this._name = name;
+    this.validate();
+  }
+
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
   }
 
   activate() {
+    if (this._address.length === 0) {
+      throw new Error("Address is mandatory to activate customer");
+    }
     this._active = true;
   }
 
